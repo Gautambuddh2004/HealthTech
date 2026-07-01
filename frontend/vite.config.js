79799fa487a2550cca40473e3d0167cf2f61cfd5
Gautambuddh2004 <guptagautambuddh@gmail.com>
2026-07-01 22:13:25 +0530

    Add lucide-react dependency

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(),tailwindcss()],
    server: {
     proxy: {
-      "/api": "https://healthtech-backend-m2dv.onrender.com"
+      "/api": "http://localhost:5000"
     }
   },
   resolve: {
